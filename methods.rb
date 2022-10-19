8f355a2f3f04475febfac02a7ab614fbd04aed21
Victor Mayi <victormay.i.797@gmail.com>
2022-10-19 10:53:33 +0300

    Completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,24 @@
-# Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(programmer)
+    puts "Hello, #{programmer}!"
+end
+
+def greet_with_default(programmer="programmer")
+    puts "Hello, #{programmer}!"
+end
+
+def add(num1, num2)
+    num1 + num2
+end
+
+def halve(num)
+    if num.class != Integer
+        return nil
+    end
+
+    num/2
+end
